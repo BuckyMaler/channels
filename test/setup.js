b472a9b36d5e10ac2a16295a424411f1829972b2
Bucky Maler <maler.bucky@gmail.com>
2018-01-14 00:36:48 -0600

    Removed jsdom from test setup file

--- test/setup.js
+++ test/setup.js
@@ -1,11 +1,8 @@
 import 'raf/polyfill';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { JSDOM } from 'jsdom';
 
-global.document = new JSDOM('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
-global.navigator = global.window.navigator;
 window.localStorage = window.sessionStorage = {
   getItem(key) {
     return this[key];
